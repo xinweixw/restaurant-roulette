cf950c0543c44d772495596990f33ff4c9ec9a70
cccxw <chanxinwei3@gmail.com>
2024-06-18 23:49:11 +0800

    Improve the food review feature by adding update function and enabling only deletion of own reviews

---
+++ frontend/src/FoodReview/UpdateReviewPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import UpdateReview from './UpdateReview';
+
+const UpdateReviewPage = () => {
+  return (
+    <div>
+        <h1>Update Review Page</h1>
+        <UpdateReview />
+    </div>
+
+  )
+}
+
+export default UpdateReviewPage;
